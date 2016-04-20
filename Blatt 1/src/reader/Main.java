3da802ee40629d84cb69b49df785a6666019b0a3
Fabian Sperrle <fabian.sperrle@gmail.com>
2016-04-20 16:12:05 +0200

    Added tokenizer

--- Blatt 1/src/reader/Main.java
+++ Blatt 1/src/reader/Main.java
@@ -2,6 +2,9 @@ package reader;
 import java.io.IOException;
 import java.util.List;
 
+import tokenizer.MissingInputException;
+import tokenizer.Tokenizer;
+
 public class Main {
 
 	public static void main(String[] args) throws IOException{
@@ -9,5 +12,18 @@ public class Main {
 		Reader reader = new Reader(filePath);
 		
 		List<Review> reviews = reader.readFile();
+		
+		Review r = reviews.get(1);
+		System.out.println(r.getText());
+		Tokenizer tokenizer = new Tokenizer(r.getText());
+		try {
+			String[] result = tokenizer.tokenize();
+			for (String str : result) {
+				System.out.println(str);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 }
---
+++ Blatt 1/src/tokenizer/Tokenizer.java
@@ -0,0 +1,50 @@
+package tokenizer;
+
+import java.io.IOException;
+import java.io.StreamTokenizer;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Tokenizer {
+
+	private StreamTokenizer st;
+	
+	public Tokenizer(String text) {
+		this.st = new StreamTokenizer(new StringReader(text));
+		this.st.quoteChar('"');
+	}
+	
+	
+	private String removeTrailingPeriod(String token) {
+		if (token.endsWith(".")) {
+			token = token.substring(0, token.length()-1);
+		}
+		return token;
+	}
+	
+	public String[] tokenize() throws IOException {
+		List<String> results = new ArrayList<>();
+		
+		while(this.st.nextToken() != StreamTokenizer.TT_EOF) {
+			switch (st.ttype) {
+			case StreamTokenizer.TT_WORD:
+				results.add(st.sval);
+				break;
+			case '"':
+			case '\'':
+				results.addAll(Arrays.asList(new Tokenizer(st.sval).tokenize()));
+				break;
+			default:
+				break;
+			}
+		}
+
+		results = results.stream().map(token -> removeTrailingPeriod(token)).collect(Collectors.toList());
+		return results.toArray(new String[0]);
+	}
+	
+}
