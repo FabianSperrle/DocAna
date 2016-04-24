ee5bf6e256858cc2d17896ae35109c35b7fda291
Fabian Sperrle <fabian.sperrle@gmail.com>
2016-04-24 14:14:05 +0200

    whaaat

--- Blatt 1/src/tokenizer/Tokenizer.java
+++ Blatt 1/src/tokenizer/Tokenizer.java
@@ -6,7 +6,6 @@ import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Tokenizer {
