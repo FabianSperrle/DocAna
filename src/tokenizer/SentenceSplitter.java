e1c9a65c2eeeb0424dc6c93110281dbf4f09a84e
Fabian Sperrle <fabian.sperrle@gmail.com>
2016-06-11 08:52:34 +0200

    Rename SplitSentences

---
+++ src/tokenizer/SentenceSplitter.java
@@ -0,0 +1,21 @@
+package tokenizer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SentenceSplitter {
+
+	public String[] split(String s) throws IOException {
+		Pattern stuff = Pattern.compile("\\S(?:.|\\n)*?(?:\\.|\\?|!|:)(?=(?:\\s+[A-Z])|\\Z)");
+		Matcher matcher = stuff.matcher(s);
+		List<String> sentences = new ArrayList<>();
+		while (matcher.find()) {
+			sentences.add(matcher.group(0)); // add match to the list
+		}
+		return sentences.toArray(new String[0]);
+	}
+
+}
