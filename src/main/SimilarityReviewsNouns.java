207b5be04bfad74a34e603720c32caa13550b3c6
Fabian Sperrle <fabian.sperrle@uni.kn>
2016-06-20 18:35:01 +0200

    Add missing similarity measures

---
+++ src/main/SimilarityReviewsNouns.java
@@ -0,0 +1,55 @@
+package main;
+
+import pos.brill.BrillTagger;
+import similarity.TF_IDF;
+import stemmer.KehlbeckSperrleStemmer;
+import tokenizer.Tokenizer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class SimilarityReviewsNouns {
+    private List<String> tagSet;
+    private Tokenizer tokenizer;
+    private BrillTagger bt;
+
+    public SimilarityReviewsNouns() {
+        try {
+            tagSet = Files.readAllLines(Paths.get("data/brill/taglist.txt"));
+            bt = new BrillTagger();
+            tokenizer = new Tokenizer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public double[][] getNounSimilarity(List<String> texts) {
+        KehlbeckSperrleStemmer st = new KehlbeckSperrleStemmer();
+        final List<String> np = texts.stream()
+                .map(text -> {
+                    String[] tokens = tokenizer.tokenize(text);
+                    String[] tags = bt.tag(tokens);
+
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < tokens.length; i++) {
+                        if (tags[i] == null) {
+                            continue;
+                        }
+                        if (tags[i].contains("np")) {
+                            sb.append(st.stem(tokens[i]));
+                        }
+                    }
+
+                    return sb.toString();
+                })
+                .collect(Collectors.toList());
+
+        return new TF_IDF(np).tf_idf(np);
+    }
+}
